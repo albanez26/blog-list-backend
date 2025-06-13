1a07bee281f63df37ecf362da83e350491861c0b
Edwin Albanez <edwinalbanez23@gmail.com>
2025-06-12 22:35:44 -0600

    update

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,13 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
 const middleware = require('../utils/middleware')
-
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { name: 1, username: 1 })
-  response.json(blogs)
-})
 
 blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   const { user } = request
@@ -15,7 +8,6 @@ blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
     ...request.body,
     user: user._id
   })
-
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog.id)
   await user.save()
@@ -23,19 +15,41 @@ blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
-  const validToken = jwt.verify(
-    request.token,
-    process.env.SECRET
-  )
-  if(!validToken.id){
-    return response.status(401).json({
-      error: 'invalid token'
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { name: 1, username: 1 })
+  response.json(blogs)
+})
+
+blogsRouter.put('/:id', middleware.userExtractor, async (request, response) => {
+  const { body: blog } = request
+  const blogToUpdate = await Blog.findById(request.params.id)
+  if (!blogToUpdate) {
+    return response.status(404).json({
+      error: 'blog not found'
     })
   }
-  const blogToDelete = await Blog.findById(request.params.id)
-  const user = await User.findById(validToken.id)
 
+  const { user } = request
+  if (blogToUpdate.user.toString() !== user._id.toString()) {
+    return response.status(401).json({
+      error: 'user not authorized'
+    })
+  }
+
+  blogToUpdate.set({
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes
+  })
+
+  const updatedBlog = await blogToUpdate.save()
+  response.status(200).json(updatedBlog)
+})
+
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
+  const blogToDelete = await Blog.findById(request.params.id)
+  const { user } = request
   if(blogToDelete.user.toString() === user._id.toString()) {
     user.blogs = user.blogs.filter(blog =>
       blog._id.toString() !== blogToDelete._id.toString()
@@ -45,18 +59,9 @@ blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) =
     response.status(204).end()
   } else {
     response.status(401).json({
-      error: 'user not authorized'
+      error: 'This blog was created by another user',
     })
   }
-})
-
-blogsRouter.put('/:id', async (request, response) => {
-  const updatedBlog = await Blog.findByIdAndUpdate(
-    request.params.id,
-    request.body,
-    { new: true }
-  )
-  response.status(200).send(updatedBlog)
 })
 
 module.exports = blogsRouter
